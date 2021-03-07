fc98037b538a14e8fddebbb672b4dde6d7edc19f
Stanislav <ilchenko_stas@mail.ru>
2021-03-07 10:19:54 +0300

    !

--- idealWeight.rb
+++ idealWeight.rb
@@ -1,10 +1,10 @@
-puts "Ваше имя?"
+puts 'Ваше имя?'
 name = gets.chomp
-puts "Ваш рост?"
+puts 'Ваш рост?'
 height = gets.to_i
 ideal_weight = (height - 110) * 1.15
 if ideal_weight > 0
   puts "#{name}, ваш идеальный вес #{ideal_weight}"
 else
-  puts "Ваш вес уже оптимальный"
-end  
+  puts 'Ваш вес уже оптимальный!'
+end
